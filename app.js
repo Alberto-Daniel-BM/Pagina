8b6ef83d94e9c146ae256a5ddf88113640e51f18
Alberto-Daniel-BM <alberror810@gmail.com>
2024-09-03 22:21:21 -0600

    Pagina prueba

---
+++ app.js
@@ -0,0 +1,7 @@
+document.getElementById('no-btn').addEventListener('click', function() {
+    document.body.innerHTML = '<div style="display: flex; align-items: center; justify-content: center; height: 100vh; background-color: red; color: white; font-size: 24px;">...</div>';
+});
+
+document.getElementById('si-btn').addEventListener('click', function() {
+    document.body.innerHTML = '<div style="display: flex; align-items: center; justify-content: center; height: 100vh; background-color: #dff0d8; color: #3c763d; font-size: 24px;">Lo sabía, no puedes vivir sin mi uwu, y ciertamente...no deberías escoger la otra opción...cariño...seria lamentable, MI QUERIDA FUTURA ESPOSA~  Te amo uwu</div>';
+});
